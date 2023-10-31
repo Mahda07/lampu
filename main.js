c6c04e5a5bf941be2b309c3b0caf6f16e8a7ab98
mahdafatihah@gmail.com <mahdafatihah@gmail.com>
2023-10-31 10:04:27 +0700

    original

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+function nyalakan(){
+  document.getElementById("lampu").src = "nyala.png";
+}
+
+function matikan(){
+  document.getElementById("lampu").src = "mati.png";
+}
